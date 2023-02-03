42d9f537a59ccb1dc138b77d6f239b91bb5c3bfd
Juan Pablo Favale <juanpablofavale@hotmail.com>
2023-02-03 07:49:06 -0300

    avanzando con la edicion del portfolio

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let btnCancel = document.getElementById("cancel")
 let btnOk = document.getElementById("ok")
 let btnDelete = document.getElementsByClassName("eliminar")
 let btnEdit = document.getElementById("editar")
+let btnAdd = document.getElementById("agregar")
+let tagMain = document.getElementById("main")
 
 function visible(){
     btnCancel.classList.toggle("visible")
@@ -10,11 +12,14 @@ function visible(){
     for (i of btnDelete){
         i.classList.toggle("visible")
     }
+    btnAdd.classList.toggle("visible")
 }
 
 function editar(){
     console.log("editando")
     visible()
+    console.log(tagMain.innerHTML)
+    tagMain.setAttribute("contenteditable","true")
 }
 
 function eliminar(){
@@ -24,9 +29,15 @@ function eliminar(){
 function ok(){
     console.log("ok")
     visible()
+    tagMain.removeAttribute("contenteditable")
 }
 
 function cancel(){
     console.log("cancel")
     visible()
+    tagMain.removeAttribute("contenteditable")
+}
+
+function agregar(){
+    console.log("agregar")
 }
